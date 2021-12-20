823c1d192249a053fe3b4e6c222c8c57040a39eb
Xertyz <mkulev2003@gmail.com>
2021-12-20 12:29:36 +0300

    delete crud

--- crud.js
+++
@@ -1,9 +0,0 @@
-const repo = require("./repository")
-
-module.exports = new function(){
-    this.get = id => repo.get(id);
-    this.getAll = () => repo.getAll();
-    this.create = data => repo.create(data);
-    this.update = data => repo.update(data);
-    this.delete = id => repo.delete(id);
-}
